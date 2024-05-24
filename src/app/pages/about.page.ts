4e3535db659b474be53d5e77ca901829e8e814fd
Akinmegha Temitope Samuel <48892912+temitopeakin1@users.noreply.github.com>
2024-05-24 07:38:28 +0100

    update

---
+++ src/app/pages/about.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  template: `<h1>About Page</h1>`
+})
+
+
+
+export default class AboutComponent {
+ 
+}
